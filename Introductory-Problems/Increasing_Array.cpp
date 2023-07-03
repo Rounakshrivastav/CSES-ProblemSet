11c154ef961c177d386e2a39b404aeb345b2629b
Rounak Shrivastav <shrivastavrounak6@gmail.com>
2023-07-03 22:47:55 +0530

    Add files via upload

---
+++ Introductory-Problems/Increasing_Array.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back 
+#define mp make_pair
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+const int mod=1e9+7;
+#define endl "\n"
+#define yes cout<<"YES\n"
+#define no cout<<"NO\n"
+
+
+ void code(){
+    int n;cin>>n;
+    ll arr[n];
+    for(ll i=0;i<n;i++){
+cin>>arr[i];
+    }
+ ll ans=0;
+ ll temp=0;
+ for(ll i=1;i<n;i++){
+     
+    if(arr[i]<temp){
+      ans+=(arr[temp]-arr[i]);
+    }
+    if(temp<arr[i]){temp=arr[i];            
+    }
+ }
+ 
+ cout<<ans<<endl;
+ }
+ 
+
+
+ 
+
+
+
+int main(){
+     ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    // int t;
+    // cin>>t;
+
+    // while(t--){
+
+code();
+
+   // }
+}
